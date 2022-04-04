b3711be5d019c16562198511dddbf9ef487307bb
Michelle Fang <mfang22@terpmail.umd.edu>
2022-04-04 19:49:33 -0400

    Flask App for the UMD schedule--only front end to display search bar, table schedule, and link to all courses

---
+++ flask_app/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import InputRequired
+
+class SearchForm(FlaskForm):
+    search_query = StringField('Query', validators=[InputRequired()])
+    submit = SubmitField('Submit')
