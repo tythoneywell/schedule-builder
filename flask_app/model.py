6b3bf0faba453da62531a3c9f677840b9b8bd190
Tyler Honeywell <tythoneywell@gmail.com>
2022-04-04 20:44:36 -0400

    Added sample course data for sprint 1. Can read in data as a list of dicts.

--- flask_app/model.py
+++ flask_app/model.py
@@ -1,10 +1,16 @@
 import requests
+import json
 
 
 class MyClass(object):
+
     def __init__(self, course_code, time, professor, gpa):
         self.sess = requests.Session()
         self.course_code = course_code
         self.time = time
         self.professor = professor
         self.gpa = gpa
+
+
+class CourseList:
+    all_courses_list_raw = json.load(open("data/fall2020data.json"))
