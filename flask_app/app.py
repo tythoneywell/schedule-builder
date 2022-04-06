f0c17e6e541db3620052deac2d907912b9effc84
Andrew Schuck <aschuck@umd.edu>
2022-04-05 20:55:37 -0400

    Added backend functionality for schedule building, adding, removing classes....

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, redirect, url_for
-from model import MyClass
+from model import MySchedule, CourseList
 from forms import SearchForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "super secret key"
+course_list = CourseList()  # populate the course list from the big json file
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -16,18 +17,17 @@ def index():
     form = SearchForm()
 
     if form.validate_on_submit():
-        # Use form.search_query.data to access the query the user typed in 
+        # Use form.search_query.data to access the query the user typed in
         return redirect(url_for('index'))
 
-    # Example of how to render index.html
-    courses = [MyClass("CMSC131", "9AM - 10AM", "Nelson", 3.8, ), MyClass("CMSC132", "2 PM - 3 PM", "Yoon", 3.5)]
-    monday_classes = [MyClass("CMSC131", "9AM - 10AM", "Nelson", 3.8, )]
-    friday_classes = [MyClass("CMSC132", "2 PM - 3 PM", "Yoon", 3.5)]
+    schedule = MySchedule()
+    cmsc250 = course_list.courses.get("CMSC250")
+    comm107 = course_list.courses.get("COMM107")
+    schedule.add_class(cmsc250.sections[-2])  # get the 2nd to last 250 section
+    schedule.add_class(comm107.sections[-1])  # get the 1st section of comm107
 
     return render_template('index.html',
-                           courses=courses,
-                           monday_classes=monday_classes,
-                           friday_classes=friday_classes,
+                           schedule=schedule,
                            form=form)
 
 
@@ -36,8 +36,5 @@ def all_courses():
     """"
     Display a list of all courses that the student could try to sign up for
     """
-
-    # Example of how to render the all_courses.html
-    courses = [MyClass("CMSC131", "9AM - 10AM", "Nelson", 3.8), MyClass("CMSC132", "2 PM - 3 PM", "Yoon", 3.5)]
     return render_template('all_courses.html',
-                           courses=courses)
+                           courses=course_list.courses)
--- flask_app/model.py
+++ flask_app/model.py
@@ -2,15 +2,95 @@ import requests
 import json
 
 
-class MyClass(object):
-
-    def __init__(self, course_code, time, professor, gpa):
+class Course(object):
+    def __init__(self, course_code: str, name: str, course_credits: int, sections: list):
         self.sess = requests.Session()
         self.course_code = course_code
-        self.time = time
+        self.name = name
+        self.credits = course_credits
+        self.sections = sections
+
+
+# class which includes information of a section from a specific course
+class Section(object):
+    def __init__(self, course_code: str, section_id: str, total_seats: int, open_seats: int, class_meetings: list,
+                 professor: str, gpa: float, course: Course):
+        self.sess = requests.Session()
+        self.course_code = course_code
+        self.section_id = section_id
+        self.total_seats = total_seats
+        self.open_seats = open_seats
+        self.class_meetings = class_meetings
         self.professor = professor
         self.gpa = gpa
+        self.course = course
 
 
-class CourseList:
-    all_courses_list_raw = json.load(open("data/fall2020data.json"))
+class MySchedule(object):
+    def __init__(self):
+        self.schedule = {"M": [],
+                         "Tu": [],
+                         "W": [],
+                         "Th": [],
+                         "F": []}
+        self.total_credits = 0
+        self.class_list = []
+
+    def add_class(self, class_to_add: Section):
+        if class_to_add in self.class_list:
+            return
+
+        self.total_credits += class_to_add.course.credits
+        for class_meetings in class_to_add.class_meetings:
+            if "M" in class_meetings["days"]:
+                self.schedule["M"].append(class_to_add)
+            if "Tu" in class_meetings["days"]:
+                self.schedule["Tu"].append(class_to_add)
+            if "W" in class_meetings["days"]:
+                self.schedule["W"].append(class_to_add)
+            if "Th" in class_meetings["days"]:
+                self.schedule["Th"].append(class_to_add)
+            if "F" in class_meetings["days"]:
+                self.schedule["F"].append(class_to_add)
+        self.class_list.append(class_to_add)
+
+    def remove_class(self, class_to_remove: Section):
+        class_previously_in_schedule = False
+        for day, classes in self.schedule.items():
+            new_day_list = []
+            for one_class in classes:
+                if one_class != class_to_remove:
+                    new_day_list.append(one_class)
+                else:
+                    class_previously_in_schedule = True
+            self.schedule[day] = new_day_list
+        if class_previously_in_schedule:
+            self.class_list.remove(class_to_remove)
+            self.total_credits -= class_to_remove.course.credits
+
+
+class CourseList(object):
+    def __init__(self):
+        all_courses_list_raw = json.load(open("data/fall2020data.json"))
+        self.courses = {}  # dictionary mapping course name (string) to a Course object
+        for courses in all_courses_list_raw:
+            course_id = courses["course_id"]
+            course_name = courses["name"]
+            course_credits = int(courses["credits"])
+            sections = courses["sections"]
+            section_list = []
+            course_obj = Course(course_id, course_name, course_credits, section_list)
+
+            # populate the sections of the specific course
+            for section in sections:
+                section_id = section["section_id"]
+                total_seats = int(section["seats"])
+                open_seats = int(section["open_seats"])
+                class_meetings = section["meetings"]
+                professor = section["instructors"]
+                gpa = 3.5
+                section_list.append(
+                    Section(course_id, section_id, total_seats, open_seats, class_meetings, professor, gpa, course_obj))
+
+            self.courses[course_id] = course_obj
+
