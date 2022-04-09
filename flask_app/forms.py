bb05ccd04f183291b74f13c441d53c0280741c76
Tyler Honeywell <thoneywe@umd.edu>
2022-04-09 18:27:22 -0400

    Add full frontend functionality to add/remove classes

---
+++ flask_app/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import InputRequired
+
+
+class SearchForm(FlaskForm):
+    search_query = StringField('Query', validators=[InputRequired()])
+    submit = SubmitField('Submit')
+
+
+class AddClassForm(FlaskForm):
+    course_code = StringField('Course Code', validators=[InputRequired()])
+    section_num = StringField('Section Number', validators=[InputRequired()])
+    add_course = SubmitField('Submit')
+
+
+class AddRemoveForm(FlaskForm):
+    course_query = StringField('Course Code', validators=[InputRequired()])
+    section_query = StringField('Section Number', validators=[InputRequired()])
+    add = SubmitField('Add')
+    remove = SubmitField('Remove')
+
+
+class ClearAllCoursesForm(FlaskForm):
+    clear_all = SubmitField('Clear Schedule')
