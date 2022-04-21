b5817f438ae4086521d1c2f6718ac0d1ebaf24c6
Andrew <amschuck15@gmail.com>
2022-04-21 18:50:41 -0400

    Added tests to increase code coverage, fixed PEP errors and other minor syntax edits.

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, HiddenField
+from wtforms import StringField, SubmitField
 from wtforms.validators import InputRequired
 
 
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,5 +1,5 @@
 import unittest
-from flask_app.backend.courses import CourseList
+from flask_app.backend.courses import CourseList, APIGet
 from tests.utils import TestUtils
 
 test_util_instance = TestUtils()
@@ -43,9 +43,54 @@ class CourseListTest(unittest.TestCase):
 
 
 class CourseTest(unittest.TestCase):
+    """
+    Test functions in the classes related to Courses (e.g. Section, Course, MeetingTime, etc.). It does not test
+    the API calls
+    """
+
     def test_class_formatted_weekly_schedule_correct(self):
         cmsc250 = test_util_instance.courses["CMSC250"].sections["0307"]
         comm107 = test_util_instance.courses["COMM107"].sections["FC04"]
         self.assertEqual({'8:00am-8:50am': 'MW', '3:30pm-4:45pm': 'TuTh'},
                          cmsc250.get_formatted_weekly_schedule())
         self.assertEqual({'4:30pm-5:45pm': 'MW'}, comm107.get_formatted_weekly_schedule())
+
+    def test_meeting_time_not_equal_to_non_meeting_time_object(self):
+        cmsc250 = test_util_instance.courses["CMSC250"].sections["0307"]
+        meeting_times_dict = cmsc250.class_meetings
+        self.assertNotEqual(meeting_times_dict["M"][0], "meeting time string")
+
+    def test_meeting_times_are_equal_according_to_eq_function(self):
+        cmsc250 = test_util_instance.courses["CMSC250"].sections["0307"]
+        meeting_times_dict = cmsc250.class_meetings
+        self.assertEqual(meeting_times_dict["M"][0], meeting_times_dict["M"][0])
+
+    def test_meeting_times_are_not_equal_according_to_eq_function(self):
+        cmsc250 = test_util_instance.courses["CMSC250"].sections["0307"]
+        comm107 = test_util_instance.courses["COMM107"].sections["FC04"]
+        meeting_times_cmsc250_dict = cmsc250.class_meetings
+        meeting_times_comm107_dict = comm107.class_meetings
+        self.assertNotEqual(meeting_times_cmsc250_dict["M"][0], meeting_times_comm107_dict["M"][0])
+
+    def test_hashes_of_different_meetings_times_are_different_too(self):
+        cmsc250 = test_util_instance.courses["CMSC250"].sections["0307"]
+        comm107 = test_util_instance.courses["COMM107"].sections["FC04"]
+        meeting_times_cmsc250_dict = cmsc250.class_meetings
+        meeting_times_comm107_dict = comm107.class_meetings
+        self.assertNotEqual(hash(meeting_times_cmsc250_dict["M"][0]), hash(meeting_times_comm107_dict["M"][0]))
+
+    def test_hashes_of_same_meetings_times_are_same(self):
+        cmsc250 = test_util_instance.courses["CMSC250"].sections["0307"]
+        meeting_times_cmsc250_dict = cmsc250.class_meetings
+        self.assertEqual(hash(meeting_times_cmsc250_dict["M"][0]), hash(meeting_times_cmsc250_dict["M"][0]))
+
+
+class APIGetAndParseTest(unittest.TestCase):
+    """
+    Test functions in the APIGet and APIParse classes, which consist of several static helper methods to call
+    the Planetterp and umdio APIs, and parse the returned data into objects
+    """
+
+    def test_get_course_heads_make_sure_all_courses_we_expect_are_present(self):
+        cmsc_courses = APIGet.get_course_heads_by_query("CMSC")
+        self.assertEqual(len(cmsc_courses), 30)  # 30 CMSC courses offered this semester
