93b000180a60a1afbf9ef2ecc1a4f4ed64f4721c
Michelle Fang <mfang22@terpmail.umd.edu>
2022-04-22 17:22:41 -0400

    Deleted too much

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,3 +4,8 @@ import pytest
 def test_index_returns_200(client):
     resp = client.get("/")
     assert resp.status_code == 200
+
+
+def test_all_courses_returns_200(client):
+    resp = client.get("/all_courses/1")
+    assert resp.status_code == 200
