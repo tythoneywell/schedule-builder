34650fc0f53f819befab185c128ad0ef3e2bd082
Michelle Fang <mfang22@terpmail.umd.edu>
2022-04-22 17:22:08 -0400

    Removed test for clear schedule form

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,12 +4,3 @@ import pytest
 def test_index_returns_200(client):
     resp = client.get("/")
     assert resp.status_code == 200
-
-
-def test_clear_all_courses(client):
-    clear_classes = SimpleNamespace(clear_all="Clear Schedule")
-    form = ClearAllCoursesForm(formdata=None, obj=clear_classes)
-    response = client.post("/", data=form.data, follow_redirects=True)
-
-    assert response.status_code == 200
-    assert b"Schedule is already empty" in response.data
