8b0ab9166e4ad478b18a3b8afaf887669c03aa73
Michelle Fang <mfang22@terpmail.umd.edu>
2022-05-04 12:56:58 -0400

    Michelle scrum 2

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,3 +9,22 @@ def test_index_returns_200(client):
 def test_all_courses_returns_200(client):
     resp = client.get("/all_courses/1")
     assert resp.status_code == 200
+
+
+def test_all_professors_returns_200(client):
+    resp = client.get("/all_professors/1")
+    assert resp.status_code == 200
+
+def test_professor_detail_returns_200(client):
+    resp = client.get("/professor/%20Auguste%20Gezalyan/gezalyan")
+    assert resp.status_code == 200
+
+    resp = client.get("/professor/Aaron%20Swanlek/swanlek")
+    assert resp.status_code == 200
+
+def test_professor_detail_returns_404_if_no_professor(client):
+    resp = client.get("/professor/non_existant")
+    assert resp.status_code == 404
+
+    resp = client.get("/professor/")
+    assert resp.status_code == 404
